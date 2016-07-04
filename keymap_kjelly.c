95961cdb3a5a8de5d0b40ad11933e765044b6e80
Kuo-tung Kao <ya790206@gmail.com>
2016-07-04 19:13:05 +0800

    refactor keymap

--- keymap_kjelly.c
+++ keymap_kjelly.c
@@ -20,19 +20,19 @@
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0 */
     KEYMAP_KJELLY(
-        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MS_WH_LEFT, MS_WH_DOWN, MS_WH_UP, MS_WH_RIGHT, \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,  P,   LBRC,RBRC,BSPC,      \
-        FN1,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN13,   QUOT,     ENT,       \
-  LCTL, LSFT, Z,   X,   C,   V,   B,   N,   M,   COMM, DOT,   SLSH, FN11, FN1,       \
-        FN12,FN23, FN22,          SPC,                LEFT, DOWN, UP,   RIGHT),
+         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MS_WH_LEFT,  KC_MS_WH_DOWN,  KC_MS_WH_UP,  KC_MS_WH_RIGHT, \
+         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,      \
+         KC_FN1, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_FN13,    KC_QUOT,      KC_ENT,       \
+   KC_LCTL,  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,    KC_SLSH,  KC_FN11,  KC_FN1,       \
+         KC_FN12, KC_FN23,  KC_FN22,           KC_SPC,                 KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT),
 
     /* Layer 1 */
     KEYMAP_KJELLY(
-        GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, FN21, FN19,  \
-        CAPS,GRV,FN9,BSLS,FN10,NO  ,NO,   MINS, FN17,EQL,   FN16, INS, PAUS,DEL,      \
-        TRNS,VOLD,VOLU,MUTE,PGDN,HOME,LEFT, DOWN, UP,  RIGHT, BSPC, DEL, PENT,
-   TRNS,TRNS,APP,NLCK,PSCR,END,PGUP,FN10,FN0,FN0, FN0, FN9, RSFT,TRNS,      \
-        TRNS,DEL,TRNS,          TRNS,            RALT,RGUI,RSFT,RCTL),
+         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_FN21,  KC_FN19,  \
+         KC_CAPS, KC_GRV, KC_FN9, KC_BSLS, KC_FN10,KC_NO  , KC_NO,    KC_MINS,  KC_FN17, KC_EQL,    KC_FN16,  KC_INS,  KC_PAUS, KC_DEL,      \
+         KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_PGDN, KC_HOME, KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT,  KC_BSPC,  KC_DEL,  KC_PENT,
+    KC_TRNS, KC_TRNS, KC_APP, KC_NLCK, KC_PSCR, KC_END, KC_PGUP, KC_FN10, KC_FN0, KC_FN0,  KC_FN0,  KC_FN9,  KC_RSFT, KC_TRNS,      \
+         KC_TRNS, KC_DEL, KC_TRNS,           KC_TRNS,             KC_RALT, KC_RGUI, KC_RSFT, KC_RCTL),
 
 };
 
