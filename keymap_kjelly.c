23c6ff256dc356b740753cf8e1eaf3e8b409974a
Kuo-tung Kao <ya790206@gmail.com>
2016-07-04 19:19:12 +0800

    enable dual role for "zxc,./"

--- keymap_kjelly.c
+++ keymap_kjelly.c
@@ -1,5 +1,7 @@
 #include "keymap_common.h"
 
+#define TAPK(MOD,KEY) ACTION_MODS_TAP_KEY((MOD), (KEY))
+
 /*
  * KJELLY Layout
  */
@@ -23,7 +25,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MS_WH_LEFT,  KC_MS_WH_DOWN,  KC_MS_WH_UP,  KC_MS_WH_RIGHT, \
          KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,      \
          KC_FN1, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_FN13,    KC_QUOT,      KC_ENT,       \
-   KC_LCTL,  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,    KC_SLSH,  KC_FN11,  KC_FN1,       \
+   KC_LCTL,  KC_LSFT,  TAPK(KC_LCTL,KC_Z),    TAPK(KC_LALT,KC_X),    TAPK(KC_LGUI,KC_C),    KC_V,    KC_B,    KC_N,    KC_M,    TAPK(KC_RGUI,KC_COMM),  TAPK(KC_RALT,KC_DOT),    TAPK(KC_RCTL,KC_SLSH),  KC_FN11,  KC_FN1,       \
          KC_FN12, KC_FN23,  KC_FN22,           KC_SPC,                 KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT),
 
     /* Layer 1 */
